70c681141978b6522da5f8b66e0f6d42ca8c6263
Artem Marakulin <97353137+Jazswipe@users.noreply.github.com>
2026-03-29 12:53:49 +0300

    feat: add POST and DELETE tests

--- api_tests/test_users.py
+++ api_tests/test_users.py
@@ -20,3 +20,13 @@ def test_get_single_user():
     assert user['id'] == 2
     assert 'username' in user
     print(response.json())
+def test_user_created():
+    new_user = {
+        "name":  "Artem",
+        "job": "QA Engineer" 
+    }
+    response = requests.post(f"{BASE_URL}/posts", json=new_user)
+    assert response.status_code == 201
+def test_user_delete():
+    response = requests.delete(f"{BASE_URL}/users/1")
+    assert response.status_code == 200
